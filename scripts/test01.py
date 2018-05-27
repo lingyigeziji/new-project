2c59a15b97f40624d730d0490b7b20a5b53bd24d
lingyigeziji <lingyigeziji0122@163.com>
2018-05-27 17:15:17 +0800

    '更新'

--- scripts/test01.py
+++ scripts/test01.py
@@ -4,13 +4,13 @@ import pytest
 
 class Test_01:
 
-    @allure.step(title='测试步骤001')
+    @allure.step(title='我是测试步骤001')
     @pytest.mark.parametrize('a',[1,2,3])
     def test_001(self,a):
         assert  a !=2
 
     @pytest.mark.parametrize('b',[4,5,6])
     def test_002(self,b):
-        allure.sttach('描述','我是测试步骤test_002的描述...')
+        allure.attach('描述','我是测试步骤test_002的描述...')
         assert b != 3
 
