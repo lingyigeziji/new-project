7116939a8adf516475cec54840f95d4fea419225
lingyigeziji <lingyigeziji0122@163.com>
2018-05-27 16:18:18 +0800

    '新增一条测试步骤'

--- scripts/test01.py
+++ scripts/test01.py
@@ -1,8 +1,10 @@
+import allure
 import pytest
 
 
 class Test_01:
 
+    @allure.step(title='测试步骤001')
     @pytest.mark.parametrize('a',[1,2,3])
-    def test_abc(self,a):
+    def test_001(self,a):
         assert  a !=2
