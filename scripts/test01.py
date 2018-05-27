cdedf1d56d8683cce163eef5cf425b5273361a96
lingyigeziji <lingyigeziji0122@163.com>
2018-05-27 20:23:10 +0800

    '新增优先级'

--- scripts/test01.py
+++ scripts/test01.py
@@ -5,6 +5,7 @@ import pytest
 class Test_01:
 
     @allure.step(title='我是测试步骤001')
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     @pytest.mark.parametrize('a',[1,2,3])
     def test_001(self,a):
         assert  a !=2
